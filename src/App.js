5485b764867dd4c88d8f26df949882e9b1431b04
诚官 <c980697366@gamil.com>
2026-05-03 17:15:13 +0800

    add useCallback import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { db, auth, provider } from "./firebase";
 import {
   collection, addDoc, getDocs, query, orderBy,
